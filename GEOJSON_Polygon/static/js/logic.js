a440e0d544e53f0977e86aa2afcf01dfb135c056
sobasicallyrun <aggarwaljuh@gmail.com>
2022-10-30 13:02:25 -0400

    .

--- GEOJSON_Polygon/static/js/logic.js
+++ GEOJSON_Polygon/static/js/logic.js
@@ -10,7 +10,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -18,25 +18,25 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Street": streets,
+    "Satellite Streets": satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [streets]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-let airportData="https://raw.githubusercontent.com/aggarwaljuh/Mapping_Earthquakes/Mapping_GeoJSON_Multipoints/GeoJSONMulti/majorAirports.json"
+let torontoHoods="https://raw.githubusercontent.com/aggarwaljuh/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/GEOJSON_Polygon/torontoNeighborhoods.json"
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
