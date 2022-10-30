a21460ed924e06c63c5554b6c21fe848325db7fa
sobasicallyrun <aggarwaljuh@gmail.com>
2022-10-30 14:15:35 -0400

    step 4

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -22,6 +22,12 @@ let baseMaps = {
   "Satellite Streets": satelliteStreets
 };
 
+let earthquakes=new L.LayerGroup();
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
@@ -30,7 +36,7 @@ let map = L.map('mapid', {
   })
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the earthquake GeoJSON URL.
 let earthquake = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -93,5 +99,7 @@ function getColor(magnitude) {
       onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
-  }).addTo(map);
+  }).addTo(earthquakes);
+
+    earthquakes.addTo(map);
 });
